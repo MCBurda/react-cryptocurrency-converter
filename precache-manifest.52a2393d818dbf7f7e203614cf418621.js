93312c039ecce06dee4c5af5eeff3f07dca7418d
Maxwell Burda <maxwell.burda@gmail.com>
2020-05-23 21:05:59 +0200

    Updates

---
+++ precache-manifest.52a2393d818dbf7f7e203614cf418621.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2f2f52f9ad519562b61015e84e726e3",
+    "url": "/react-cryptocurrency-converter/index.html"
+  },
+  {
+    "revision": "9ae548a0b2c6bc11ee09",
+    "url": "/react-cryptocurrency-converter/static/css/main.8227832f.chunk.css"
+  },
+  {
+    "revision": "c944acbc1ca89e7199dd",
+    "url": "/react-cryptocurrency-converter/static/js/2.2cecd46c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-cryptocurrency-converter/static/js/2.2cecd46c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae548a0b2c6bc11ee09",
+    "url": "/react-cryptocurrency-converter/static/js/main.176a0e9c.chunk.js"
+  },
+  {
+    "revision": "b2688e51fc16bf8d009e",
+    "url": "/react-cryptocurrency-converter/static/js/runtime-main.23f57188.js"
+  },
+  {
+    "revision": "ab64626428669560e8b919f8ef758428",
+    "url": "/react-cryptocurrency-converter/static/media/coinspining looped 75percent slowed cropped.ab646264.gif"
+  }
+]);
